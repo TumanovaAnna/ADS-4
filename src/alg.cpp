376a837ed7ef743b0f57144434b8309cc8233243
TumanovaAnna <118038015+TumanovaAnna@users.noreply.github.com>
2023-03-22 19:27:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,10 @@
 // Copyright 2021 NNTU-CS
 
 int countPairs1(int *arr, int len, int value) {
-  int count1=0;
-  for (int i=0; i<len-1; i++) {
-    for (int j=i+1; j<len; j++) {
-      if (arr[i]+arr[j] == value) {
+  int count1 = 0;
+  for (int i = 0; i < len-1; i++) {
+    for (int j = i + 1; j < len; j++) {
+      if (arr[i] + arr[j] == value) {
         count1++;
       }
     }
@@ -12,10 +12,10 @@ int countPairs1(int *arr, int len, int value) {
   return count1;
 }
 int countPairs2(int *arr, int len, int value) {
-  int count2=0;
-  for (int i=0; i<len-1; i++) {
-    for (int j=len-1; i<j; j--) {
-      if (arr[i]+arr[j] == value) {
+  int count2 = 0;
+  for (int i = 0; i < len - 1; i++) {
+    for (int j = len - 1; i < j; j--) {
+      if (arr[i] + arr[j] == value) {
         count2++;
       }
     }
@@ -23,29 +23,29 @@ int countPairs2(int *arr, int len, int value) {
   return count2;
 }
 int countPairs3(int *arr, int len, int value) {
-  int count3=0;
-  for (int i=0; i<len-1; i++) {
-    int rig=len, lef=i;
-    while (rig-1>lef) {
-      int mids=(lef + rig) / 2;
-      if (arr[i]+arr[mids] == value) {
+  int count3 = 0;
+  for (int i = 0; i < len - 1; i++) {
+    int rig = len, lef = i;
+    while (rig - 1 > lef) {
+      int mids = (lef + rig) / 2;
+      if (arr[i] + arr[mids] == value) {
         count3++;
-        int midd=mids+1;
-        while (arr[i]+arr[midd] == value && midd<rig) {
+        int midd = mids + 1;
+        while (arr[i] + arr[midd] == value && midd < rig) {
           count3++;
           midd++;
         }
-        midd=mids-1;
-        while (arr[i]+arr[midd] == value && midd>lef) {
-          count++;
+        midd = mids - 1;
+        while (arr[i] + arr[midd] == value && midd > lef) {
+          count3++;
           midd--;
         }
         break;
       }
-      if (arr[i]+arr[mids] > value) {
-        rig=mids;
+      if (arr[i] + arr[mids] > value) {
+        rig = mids;
       } else {
-        lef=mids;
+        lef = mids;
       }
     }
   }
